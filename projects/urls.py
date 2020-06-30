60ebde83d2b076d44554c4bda8d7634fe9aad184
NimaAram <aramnima50@gmail.com>
2020-06-30 18:53:57 +0430

    better UI/UX for homepage & addded create project url

--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = 'project'
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('create',views.create,name='create'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 
 def home(request):
     return render(request,'site/index.html')
+
+
+def create(request):
+    if request.user.is_authenticated:
+        pass
+    else:
+        return redirect('project:home')    
